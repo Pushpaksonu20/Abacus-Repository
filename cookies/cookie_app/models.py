bc69711a6fce9c5d09a398194a68c1f2488ccc4f
Pushpaksonu20 <pushpak.lsonu@gmail.com>
2025-12-19 14:42:49 +0530

    sample commit

--- cookies/cookie_app/models.py
+++ cookies/cookie_app/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Student(models.Model):
     name=models.CharField(max_length=20)
     age = models.IntegerField()
+
+class Student2(models.Model):
+    name=models.CharField(max_length=20)
+    age = models.IntegerField()
